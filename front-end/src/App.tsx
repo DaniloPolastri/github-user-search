2670453b30eb610ee170eae7691cc9db1e8d82cf
Danilo <danilopolastri.ti@gmail.com>
2021-04-20 19:20:29 -0300

    Base do projeto

---
+++ front-end/src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import './app.scss'
+
+const App = () => {
+
+  /*
+  * counter e o valor do estado
+  * setcounter - funcao que vai alterar o estado
+  * */
+
+  return (
+      <div className="box-red">
+        <h1>Testando</h1>
+      </div>
+
+  );
+}
+
+export default App;
